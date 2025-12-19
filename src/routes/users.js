8e08a714469e2a14488e140d527f62a4137fb845
hakkinev <vinski.hakkinen@gmail.com>
2025-12-19 23:33:41 +0200

    /refresh och /logout

--- src/routes/users.js
+++ src/routes/users.js
@@ -70,6 +70,86 @@ router.post('/login', async (req, res) => {
     }
 })
 
+// klienten skickar refresh token i authHeader och returnerar en ny access token (15min)
+router.post('/refresh', async (req, res) => {
+
+    try {
+        const authHeader = req.headers['authorization']
+        const token = authHeader?.split(' ')[1]
+
+        if (!token) {
+            return res.status(401).send({ msg: "Missing refresh token" })
+        }
+
+        // verifiera jwt med signatur och expiration
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+
+        // måste vara refresh token
+        if (payload.typ !== "refresh") {
+            return res.status(401).send({ msg: "Wrong token type" })
+        }
+
+        // kolla att refresh token finns i db
+        const row = await prisma.refreshToken.findUnique({
+            where: { token: token }
+        })
+
+        if (!row) {
+            return res.status(401).send({ msg: "Refresh token not found" })
+        }
+
+        // db expiration tid
+        if (row.expires_at < new Date()) {
+            return res.status(401).send({ msg: "Refresh token expired" })
+        }
+
+        // skapa ny access token
+        const newAccessToken = jwt.sign({
+            sub: payload.sub,
+            typ: "access"
+        },
+            process.env.JWT_SECRET, { expiresIn: '15m' })
+
+        res.send({
+            msg: "New access token",
+            accessToken: newAccessToken
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(401).send({
+            msg: "Refresh failed",
+            error: error.message
+        })
+    }
+})
+
+// klienten skickar refresh token i authHeader ochraderar refresh token från db
+router.post('/logout', async (req, res) => {
+    try {
+        const authHeader = req.headers['authorization']
+        const token = authHeader?.split(' ')[1]
+
+        if (!token) {
+            return res.status(400).send({ msg: "Missing token" })
+        }
+
+        // radera raden med samma token från db
+        await prisma.refreshToken.deleteMany({
+            where: { token: token }
+        })
+
+        res.send({ msg: "Logged out" })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            msg: "Logout failed",
+            error: error.message
+        })
+    }
+})
+
 router.post('/', async (req, res) => {
 
     try {
